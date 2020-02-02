af5382084df900bfc5a87fb1fada14673e98fcf3
hellomyheart <31239692+hellomyheart@users.noreply.github.com>
2020-02-03 00:01:39 +0800

    init commit

--- hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/demo/apache/dubbo/provider/service/EchoServiceImpl.java
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/demo/apache/dubbo/provider/service/EchoServiceImpl.java
@@ -2,11 +2,17 @@ package com.demo.apache.dubbo.provider.service;
 
 import com.demo.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
+
 //这里的注解使用dubbo的Service
 @Service(version = "1.0.0")
-public class EchoServiceImpl  implements EchoService {
+public class EchoServiceImpl implements EchoService {
+    @Value("${dubbo.protocol.port}")
+    private String port;
+
+
     @Override
     public String echo(String string) {
-        return "Echo Hello "+string;
+        return "Echo Hello " + string+"  port:"+port;
     }
 }
