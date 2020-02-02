b1bda62c137514b56ae08aba4eb8ea31a35fc9f2
hellomyheart <31239692+hellomyheart@users.noreply.github.com>
2020-02-03 00:01:39 +0800

    init commit

--- hello-apache-dubbo-consumer/src/main/java/com/demo/apache/dubbo/consumer/controller/EchoController.java
+++ hello-apache-dubbo-consumer/src/main/java/com/demo/apache/dubbo/consumer/controller/EchoController.java
@@ -2,18 +2,24 @@ package com.demo.apache.dubbo.consumer.controller;
 
 import com.demo.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class EchoController {
     //dubbo提供的注解
     @Reference(version = "1.0.0")
     private EchoService echoService;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping(value = "/echo/{string}")
     public String echo(@PathVariable String string) {
-        return echoService.echo(string);
+        return echoService.echo(string)+"   "+username;
     }
 }
