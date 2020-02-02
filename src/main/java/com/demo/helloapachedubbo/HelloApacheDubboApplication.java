7c77e3d439003f76d07e50b49b9a9a926c88b7c9
申永杰 <stepenshen@foxmail.com>
2020-02-02 12:14:07 +0000

    Initial commit

---
+++ src/main/java/com/demo/helloapachedubbo/HelloApacheDubboApplication.java
@@ -0,0 +1,13 @@
+package com.demo.helloapachedubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApacheDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApacheDubboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/helloapachedubbo/HelloApacheDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.helloapachedubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApacheDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
